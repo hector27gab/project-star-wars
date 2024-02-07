7a3f142aa2c2832e4b1ce4b096c8b97f601b7e72
hector27gab <hectorgabriel270405@gmail.com>
2024-02-06 23:11:47 -0400

    update

--- src/js/component/Characters.jsx
+++ src/js/component/Characters.jsx
@@ -6,43 +6,41 @@ const Characters = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="col-4 justify-content-start mt-3 ronunded">
+    <div className="justify-content-start mt-3 ronunded">
       <div>
-        <h1 className="container bg-black fw-bold w-50 rounded">Characters</h1>
+        <h1 className="bg-black fw-bold w-100 rounded">Characters</h1>
       </div>
+      <div className="d-flex overflow-auto">
+        {store.infoCharacters.map((item) => {
+          console.log(item);
+          return (
+            <div className="mx-2 mt-4" key={item.result.uid + "characters"}>
+              <div class="card card-star-wars bg-black">
+                <img
+                  src={`https://starwars-visualguide.com/assets/img/characters/${item.result.uid}.jpg`}
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">{item.result.properties.name}</h5>
 
-      {store.infoCharacters.map((item) => {
-        console.log(item);
-        return (
-          <div
-            className="container w-70 mt-4"
-            key={item.result.uid + "characters"}
-          >
-            <div class="card bg-black">
-              <img
-                src={`https://starwars-visualguide.com/assets/img/characters/${item.result.uid}.jpg`}
-                class="card-img-top"
-              />
-              <div class="card-body">
-                <h5 class="card-title">{item.result.properties.name}</h5>
-
-                <button
-                  className="btn btn-outline-danger mx-2"
-                  onClick={() => actions.addFavorite(item)}
-                >
-                  <i class="fa-regular fa-heart"></i>
-                </button>
-                <Link
-                  to={`/details/characters/${item.result.uid}`}
-                  className="btn btn-outline-primary my-2"
-                >
-                  <i className="fa-solid fa-eye"></i>
-                </Link>
+                  <button
+                    className="btn btn-outline-danger mx-2"
+                    onClick={() => actions.addFavorite(item)}
+                  >
+                    <i class="fa-regular fa-heart"></i>
+                  </button>
+                  <Link
+                    to={`/details/characters/${item.result.uid}`}
+                    className="btn btn-outline-primary my-2"
+                  >
+                    <i className="fa-solid fa-eye"></i>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/js/component/Ships.jsx
+++ src/js/component/Ships.jsx
@@ -6,39 +6,43 @@ const Ships = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="col-4 justify-content-center mt-3">
+    <div className="justify-content-center mt-3">
       <div>
-        <h1 className="container bg-black fw-bold w-50 rounded">Ships</h1>
+        <h1 className="bg-black fw-bold w-100 rounded">Ships</h1>
       </div>
+      <div className="d-flex overflow-auto">
+        {store.infoShips.map((item) => {
+          return (
+            <div
+              className="container w-70 mt-4"
+              key={item.result.uid + "ships"}
+            >
+              <div class="card card-star-wars bg-black">
+                <img
+                  src={`https://starwars-visualguide.com/assets/img/vehicles/${item.result.uid}.jpg`}
+                  class="card-img-top"
+                />
+                <div class="card-body">
+                  <h5 class="card-title">{item.result.properties.name}</h5>
 
-      {store.infoShips.map((item) => {
-        return (
-          <div className="container w-70 mt-4" key={item.result.uid + "ships"}>
-            <div class="card bg-black">
-              <img
-                src={`https://starwars-visualguide.com/assets/img/vehicles/${item.result.uid}.jpg`}
-                class="card-img-top"
-              />
-              <div class="card-body">
-                <h5 class="card-title">{item.result.properties.name}</h5>
-
-                <button
-                  className="btn btn-outline-danger mx-2"
-                  onClick={() => actions.addFavorite(item)}
-                >
-                  <i class="fa-regular fa-heart"></i>
-                </button>
-                <Link
-                  to={`/details/ships/${item.result.uid}`}
-                  className="btn btn-outline-primary my-2"
-                >
-                  <i class="fa-solid fa-eye"></i>
-                </Link>
+                  <button
+                    className="btn btn-outline-danger mx-2"
+                    onClick={() => actions.addFavorite(item)}
+                  >
+                    <i class="fa-regular fa-heart"></i>
+                  </button>
+                  <Link
+                    to={`/details/ships/${item.result.uid}`}
+                    className="btn btn-outline-primary my-2"
+                  >
+                    <i class="fa-solid fa-eye"></i>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
